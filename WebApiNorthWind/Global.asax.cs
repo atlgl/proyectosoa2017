4ca1fdc802e4134d6c8c6f0cad302506bf6a9fc5
atlgl <atl_gl@hotmail.com>
2017-06-20 09:06:51 -0500

    p
    Api RESt

---
+++ WebApiNorthWind/Global.asax.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Routing;
+
+namespace WebApiNorthWind
+{
+    public class WebApiApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            GlobalConfiguration.Configure(WebApiConfig.Register);
+            //Agregar estas lineas para trabajar con el Entity Framework y enviar JSON nada mas
+            GlobalConfiguration.Configuration.Formatters.JsonFormatter.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore;
+            GlobalConfiguration.Configuration.Formatters.Remove(GlobalConfiguration.Configuration.Formatters.XmlFormatter);
+            //Es altamente recomendable no pasar directamente las entidades ,si no crear
+            //un UserModel en la carpeta de los modelos por que se puede decidir que se muestra y que no
+            //[KnowTypeAttribute(typeof(UserModel))] saber cual es la clase expuesta en la API REST
+
+        
+        }
+    }
+}
